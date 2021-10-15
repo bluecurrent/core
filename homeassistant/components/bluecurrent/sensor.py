07db129cc2ddf6240715fafc12b3488225595fe9
Floris272 <florispuijk@outlook.com>
2021-10-15 12:38:05 +0000

    added device put URL in const

--- homeassistant/components/bluecurrent/sensor.py
+++ homeassistant/components/bluecurrent/sensor.py
@@ -47,10 +47,18 @@ class PrototypeSensor(ChargePointEntity, SensorEntity):
         self._attr_native_unit_of_measurement = sensor[1][CONF_UNIT_OF_MEASUREMENT]
         self._attr_device_class = sensor[1][CONF_DEVICE_CLASS]
         self._attr_native_value = 0
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, self.evse_id)},
+            "name": "NanoCharge",
+            "manufacturer": "BlueCurrent",
+            "model": "v2",
+            "sw_version": "1",
+            "via_device": (DOMAIN, "test"),
+        }
 
     @callback
     def update_from_latest_data(self) -> None:
         """Fetch new state data for the sensor."""
-        self._attr_native_value = self._connector.charge_points[self._evse_id][
+        self._attr_native_value = self._connector.charge_points[self.evse_id][
             self.data_id
         ]
