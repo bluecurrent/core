483490aa1c1729cd3c608a3b6581e98cbd35337b
Floris272 <florispuijk@outlook.com>
2021-12-29 15:21:37 +0000

    removed stuff not needed for sensor platform

--- homeassistant/components/bluecurrent/const.py
+++ homeassistant/components/bluecurrent/const.py
@@ -1,49 +1,26 @@
 """Constants for the BlueCurrent integration."""
 import logging
 
-from bluecurrent_api import Client
-
-from homeassistant.components.button import ButtonEntityDescription
 from homeassistant.components.sensor import SensorEntityDescription
-from homeassistant.const import CONF_ICON, CONF_NAME, CONF_SERVICE
 
 DOMAIN = "bluecurrent"
 
 LOGGER = logging.getLogger(__package__)
 
-PLATFORMS = ["sensor", "switch", "button"]
-
-KEY = "key"
-
-CARD = "card"
-
-EVSE_ID = "evse_id"
-
+PLATFORMS = ["sensor"]
+CHARGE_POINTS = "CHARGE_POINTS"
 DATA = "data"
-RESULT = "result"
-
+DELAY = 10
+EVSE_ID = "evse_id"
+ERROR = "error"
+GRID_STATUS = "GRID_STATUS"
 MODEL_TYPE = "model_type"
 OBJECT = "object"
-SUCCESS = "success"
-ERROR = "error"
-FUNCTION = "function"
-
-CHARGE_POINTS = "CHARGE_POINTS"
-GRID_STATUS = "GRID_STATUS"
-VALUE_TYPES = ("CH_STATUS", "CH_SETTINGS")
-SERVICES = ("SOFT_RESET", "REBOOT")
-SETTINGS = ("AVAILABLE", "PUBLIC_CHARGING", "PLUG_AND_CHARGE")
-
-AVAILABLE = "available"
-UNAVAILABLE = "unavailable"
-ACTIVITY = "activity"
 TIMESTAMP_KEYS = ("start_session", "stop_session", "offline_since")
+VALUE_TYPES = "CH_STATUS"
 
 # temp
-URL = "ws://172.21.109.125:8765"
-
-DELAY = 10
-
+URL = "ws://172.21.100.249:8765"
 
 SENSORS = (
     SensorEntityDescription(
@@ -99,7 +76,6 @@ SENSORS = (
         native_unit_of_measurement="kWh",
         device_class="energy",
         name="Energy Usage",
-        # state_class="measurement",
     ),
     SensorEntityDescription(
         key="start_session",
@@ -164,63 +140,4 @@ GRID_SENSORS = (
         device_class="current",
         name="Total Grid Current",
     ),
-)
-
-BUTTONS = (
-    {
-        CONF_SERVICE: "get_status",
-        CONF_NAME: "Get status",
-        CONF_ICON: "mdi:database-arrow-down",
-    },
-    {
-        CONF_SERVICE: "reset",
-        CONF_NAME: "Reset",
-        CONF_ICON: "mdi:restart",
-    },
-    {
-        CONF_SERVICE: "reboot",
-        CONF_NAME: "Reboot",
-        CONF_ICON: "mdi:restart-alert",
-    },
-    {
-        CONF_SERVICE: "start_session",
-        CONF_NAME: "Start session",
-        CONF_ICON: "mdi:play",
-    },
-    {
-        CONF_SERVICE: "stop_session",
-        CONF_NAME: "Stop session",
-        CONF_ICON: "mdi:stop",
-    },
-)
-BUTTONS2 = (
-    ButtonEntityDescription(
-        key="get_status", name="Get status", icon="mdi:database-arrow-down"
-    ),
-    ButtonEntityDescription(key="reset", name="Get Reset", icon="mdi:restart"),
-    ButtonEntityDescription(key="reboot", name="Reboot", icon="mdi:restart-alert"),
-    ButtonEntityDescription(key="start_session", name="Start session", icon="mdi:play"),
-    ButtonEntityDescription(key="stop_session", name="Stop session", icon="mdi:stop"),
-)
-
-
-SWITCHES = (
-    {
-        KEY: "plug_and_charge",
-        FUNCTION: Client.set_plug_and_charge,
-        CONF_NAME: "Plug and charge",
-        CONF_ICON: "mdi:ev-plug-type2",
-    },
-    {
-        KEY: "public_charging",
-        FUNCTION: Client.set_public_charging,
-        CONF_NAME: "Public charging",
-        CONF_ICON: "mdi:account-group",
-    },
-    {
-        KEY: "available",
-        FUNCTION: Client.set_available,
-        CONF_NAME: "Available",
-        CONF_ICON: "mdi:power",
-    },
 )
